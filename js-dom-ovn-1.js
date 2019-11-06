6dd8fd5f08dbec2ee0798d4e5c21758d5aae5984
Dvard <edvard.busck-nielsen@edu.prakticum.fi>
2019-11-06 12:48:35 +0000

    Part 1

---
+++ js-dom-ovn-1.js
@@ -0,0 +1,11 @@
+console.log(document.querySelector('#age-table'));
+
+console.log(document.querySelectorAll('label'));
+
+console.log(document.querySelectorAll('td')[0]);
+
+console.log(document.querySelectorAll('form')[0]);
+
+console.log(document.querySelectorAll('form')[0].querySelectorAll('input')[0]);
+
+console.log(document.querySelectorAll('form')[0].querySelectorAll('input')[document.querySelectorAll('form')[0].querySelectorAll('input').length-1]);
